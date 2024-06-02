1b75caef7e493f33639fccd62dd8e49846dc6975
Ashutosh Gaur <121347667+ashutoshgaur0809@users.noreply.github.com>
2024-06-02 10:50:56 +0530

    Add files via upload

---
+++ Reverse_String.java
@@ -0,0 +1,14 @@
+class Reverse_String {
+    public void reverseString(char[] s) {
+       int l = 0;
+       int r = s.length - 1;
+       while(l<r){
+        char t = s[l];
+        s[l] = s[r];
+        s[r] = t;
+        l = l + 1;
+        r = r - 1;
+       }
+    }
+    
+}
