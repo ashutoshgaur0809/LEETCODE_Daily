cb563bb4f4302249ac529da12d269eb44ab321ed
Ashutosh Gaur <121347667+ashutoshgaur0809@users.noreply.github.com>
2024-09-04 18:04:37 +0530

    Create StringCompression.py

---
+++ StringCompression.py
@@ -0,0 +1,42 @@
+s = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+d = {}
+for i in s:
+    if i in d:
+        d[i] += 1
+    else:
+        d[i] = 1
+
+print(d)
+
+s = ""
+
+clk = 0
+for i,j in d.items():
+    if j == 1:
+        clk += 1
+    s += i+str(j)
+ 
+
+        
+                
+            
+    
+print(s)
+print(clk)
+
+
+# Example 1:
+
+# Input: chars = ["a","a","b","b","c","c","c"]
+# Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+# Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+# Example 2:
+
+# Input: chars = ["a"]
+# Output: Return 1, and the first character of the input array should be: ["a"]
+# Explanation: The only group is "a", which remains uncompressed since it's a single character.
+# Example 3:
+
+# Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+# Output: Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+# Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
